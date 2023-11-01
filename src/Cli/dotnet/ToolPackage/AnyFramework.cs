2c1bb0681645cf02d3bdca67a47119767b4fd03a
annie <59816815+JL03-Yue@users.noreply.github.com>
2023-11-01 16:38:10 -0700

    push for reference

---
+++ src/Cli/dotnet/ToolPackage/AnyFramework.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using NuGet.Frameworks;
+
+namespace NuGet.Client;
+
+internal class AnyFramework : NuGetFramework
+{
+    internal static AnyFramework Instance { get; } = new AnyFramework();
+
+
+    private AnyFramework()
+        : base(AnyFramework)
+    {
+    }
+}
